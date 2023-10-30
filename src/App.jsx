621463ea907ae541edcf64188858bb625982e9f8
Octaram <moctaramadhan12@gmail.com>
2023-10-30 15:05:13 +0700

    hoyo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import './App.css'
 import React from 'react'
+import DashboardAdmin from './pages/admin/Dashboard'
+import DashboardUser from './pages/user/dashboard'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/admin/Dashboard'
 import AttendanceOverview from './pages/admin/AttendanceOverview'
+import AttendanceHistory from './pages/user/attendance'
+import Login from './auth/Login'
+import { useUserInfo } from './use context/user-info'
+import PrivateRoute from './privat-router/privatRoute'
 function App() {
-
+  const { userInfo } = useUserInfo()
+  const role = userInfo.role
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/attendance-overview" element={<AttendanceOverview />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<PrivateRoute requiredRoles={["Admin", "Public"]} element={role === "Admin" ? <DashboardAdmin /> : <DashboardUser />} />} />
+          <Route path="/attendance-overview" element={<PrivateRoute requiredRoles={["Admin"]} element={<AttendanceOverview />} />} />
+          <Route path="/attendance-history" element={<PrivateRoute requiredRoles={"Public"} element={<AttendanceHistory />} /> } />
+        </Routes>
+      </Router>
     </>
   )
 }
